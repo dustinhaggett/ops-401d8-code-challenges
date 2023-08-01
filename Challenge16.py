05d78f01716c3939520f4f66f32b0629f5f5430c
Dustin <dustin@iroquois.capital>
2023-08-01 10:18:04 -0600

    updates

--- Challenge16.py
+++ Challenge16.py
@@ -1,35 +1,35 @@
 import time
-import getpass  # Import the getpass module
+import getpass
 
 # Part 1: Dictionary Iterator Function (Mode 1)
-# This function takes a file path as input, opens the file, and iterates through each word in the file.
-# It then prints each word to the screen with a delay of 1 second between each word.
-def dictionary_iterator(file_path):
-    with open(file_path, 'r', errors='ignore') as file:
-        for line in file:
-            word = line.strip()  # remove newline characters
-            print(word)
-            time.sleep(1)  # delay for 1 second
+def dictionary_iterator():
+    filepath = input("Enter your dictionary filepath:\n")
+    file = open(filepath, encoding="ISO-8859-1")
+    line = file.readline()
+    while line:
+        line = line.rstrip()
+        word = line
+        print(word)
+        time.sleep(1)
+        line = file.readline()
+    file.close()
 
 # Part 2: Password Recognized Function (Mode 2)
-# This function takes a string and a file path as input. It opens the file and checks if the string appears in the file.
-# If the string is found in the file, it prints a message indicating that the string was found.
-# Otherwise, it prints a message indicating that the string was not found.
-def password_recognized(string, file_path):
-    with open(file_path, 'r', errors='ignore') as file:
-        if string in file.read():
-            print(f"The string '{string}' was found in the word list.")
-        else:
-            print(f"The string '{string}' was not found in the word list.")
+def password_recognized():
+    filepath = input("Enter your dictionary filepath:\n")
+    string = getpass.getpass("Enter a string: ")
+    file = open(filepath, encoding="ISO-8859-1")
+    if string in file.read():
+        print(f"The string '{string}' was found in the word list.")
+    else:
+        print(f"The string '{string}' was not found in the word list.")
+    file.close()
 
 # Part 3: Password Complexity Function (Mode 3)
-# This function takes a password string as input and evaluates its complexity based on certain metrics.
-# The metrics checked include the length of the password, the presence of capital letters, numbers, and symbols.
-# It prints whether each metric is satisfied by the password and then gives an overall SUCCESS or FAILURE message.
 def password_complexity():
-    password = getpass.getpass("Enter a password: ")  # Use getpass to securely get the password from the user
+    password = getpass.getpass("Enter a password: ")
     metrics = {
-        'length': len(password) >= 8,  # replace 8 with your desired length
+        'length': len(password) >= 8,
         'capital_letter': any(c.isupper() for c in password),
         'number': any(c.isdigit() for c in password),
         'symbol': any(not c.isalnum() for c in password),
@@ -43,33 +43,28 @@ def password_complexity():
     else:
         print("FAILURE: The password does not meet all requirements.")
 
-# Part 4: Main Function
-# This function is the main entry point of the script.
-# It prompts the user to select a mode and takes appropriate actions based on the chosen mode.
-# For mode 1, it asks for the path to the word list file and calls the dictionary_iterator function.
-# For mode 2, it asks for a string and the path to the word list file and calls the password_recognized function.
-# For mode 3, it calls the password_complexity function.
-def main():
-    print("Select a mode:")
-    print("1: Offensive; Dictionary Iterator")
-    print("2: Defensive; Password Recognized")
-    print("3: Defensive; Password Complexity")
-    mode = int(input("Enter the number of your chosen mode: "))
-
-    if mode == 1:
-        file_path = input("Enter the path to the word list file: ")
-        dictionary_iterator(file_path)
-    elif mode == 2:
-        string = input("Enter a string: ")
-        file_path = input("Enter the path to the word list file: ")
-        password_recognized(string, file_path)
-    elif mode == 3:
-        password_complexity()
-    else:
-        print("Invalid mode selected.")
-
+# Main Function
 if __name__ == "__main__":
-    main()
+    while True:
+        mode = input("""
+Brute Force Wordlist Attack Tool Menu
+1 - Offensive, Dictionary Iterator
+2 - Defensive, Password Recognized
+3 - Defensive, Password Complexity
+4 - Exit
+Please enter a number: 
+""")
+        if mode == "1":
+            dictionary_iterator()
+        elif mode == "2":
+            password_recognized()
+        elif mode == "3":
+            password_complexity()
+        elif mode == "4":
+            break
+        else:
+            print("Invalid selection...")
+
 
 
 
