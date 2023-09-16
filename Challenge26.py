6d338930f71f5cb6da2dd509e45d2a9c55e77382
Dustin <dustin@iroquois.capital>
2023-09-16 16:41:57 -0300

    updates

---
+++ Challenge26.py
@@ -0,0 +1,15 @@
+import logging
+
+# Configure logging to write messages to a file with a certain level
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+# Different levels of log messages
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+
+
+
+# Attribution to chatgpt4 code interpreter and codefellows github
